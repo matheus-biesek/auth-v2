a5bacf2d111efb7bbd3bcfc5ad15d14e70f7fd3f
Matheus <md.biesek@gmail.com>
2025-01-31 03:48:54 -0300

    Criação do serviço para formatar mensagens de erro de validação no RestController

---
+++ src/main/java/com/mat/auth/adapters/rest/exception/ValidationErrorHandler.java
@@ -0,0 +1,18 @@
+package com.mat.auth.adapters.rest.exception;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindingResult;
+
+import java.util.stream.Collectors;
+
+public class ValidationErrorHandler {
+
+    public static String getErrorMessages(BindingResult result) {
+        if (result.hasErrors()) {
+            return result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+        }
+        return null;
+    }
+}
