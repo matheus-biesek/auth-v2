720932b3c9c58d8c798488c5afc2797663351dd9
Matheus <md.biesek@gmail.com>
2025-01-31 03:42:00 -0300

    Criação do SecurityConfig para controle de autenticação e autorização

---
+++ src/main/java/com/mat/auth/adapters/services/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.mat.auth.adapters.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/register-adm").permitAll() //.hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/auth/update-role").permitAll() //.hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/auth/delete-user").permitAll() //.hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/register-client", "/token/jwt-is-valid", "/token/jwt-is-valid-adm", "/token/jwt-is-valid-user").permitAll()
+                        .anyRequest().permitAll() //.authenticated()
+                )
+                .addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
