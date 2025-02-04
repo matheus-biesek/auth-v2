50ac4eb37b7e9e64ca1b9fa8c47ba1f5bd0ffdf6
Matheus <md.biesek@gmail.com>
2025-02-04 17:35:50 -0300

    Ajuste no nome da fila do consumidor pois não estava presente

--- src/main/java/com/mat/auth/adapters/messaging/consumer/TokenValidationConsumerImpl.java
+++ src/main/java/com/mat/auth/adapters/messaging/consumer/TokenValidationConsumerImpl.java
@@ -5,24 +5,31 @@ import com.mat.auth.application.service.TokenServiceImpl;
 import com.mat.auth.domain.dto.request.TokenValidationRequestDTO;
 import com.mat.auth.domain.port.in.TokenValidationConsumerPort;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TokenValidationConsumer implements TokenValidationConsumerPort {
+public class TokenValidationConsumerImpl implements TokenValidationConsumerPort {
 
+    private static final Logger logger = LoggerFactory.getLogger(TokenValidationConsumerImpl.class);
     private final TokenServiceImpl tokenServiceImpl;
     private final ObjectMapper objectMapper;
 
+
     @Override
+    @RabbitListener(queues = "token.validation")
     public String validateTokenForRole(String jsonMessage) {
         try {
-            TokenValidationRequestDTO request = this.objectMapper.readValue(jsonMessage, TokenValidationRequestDTO.class);
+            TokenValidationRequestDTO request = objectMapper.readValue(jsonMessage, TokenValidationRequestDTO.class);
 
-            boolean isValid = this.tokenServiceImpl.validateTokenForRole(request.getToken(), request.getRole());
+            boolean isValid = tokenServiceImpl.validateTokenForRole(request.getToken(), request.getRole());
 
             return isValid ? "VALID" : "INVALID";
         } catch (Exception e) {
+            logger.error("Erro inesparado: ", e);
             return "ERROR";
         }
     }
