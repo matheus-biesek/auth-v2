5b74746842fa8ac33bcfa17348d8f0aacb6383b1
matheus biesek <md.biesek@gmail.com>
2025-02-07 02:14:19 -0300

    Ajuste para retorna de uma forma genérica ao usuário as excessões que ocorrem no back end

--- src/main/java/com/mat/auth/adapters/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mat/auth/adapters/rest/exception/GlobalExceptionHandler.java
@@ -1,18 +1,17 @@
 package com.mat.auth.adapters.rest.exception;
 
-import com.mat.auth.domain.dto.response.MessageResponseDTO;
+import com.mat.auth.domain.dto.response.ErrorResponseDTO;
 import com.mat.auth.domain.exceptions.UserAlreadyExistsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,45 +19,45 @@ public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+    public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception ex) {
         logger.error("Erro inesperado: {}", ex.getMessage(), ex);
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Ocorreu um erro interno no servidor");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        return new ResponseEntity<>(new ErrorResponseDTO("Ocorreu um erro interno no servidor!"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>(new ErrorResponseDTO("O corpo da requisição é obrigatório!"), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<MessageResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         BindingResult bindingResult = ex.getBindingResult();
         FieldError fieldError = bindingResult.getFieldError();
 
         if (fieldError != null) {
             String errorMessage = fieldError.getDefaultMessage();
-            MessageResponseDTO response = new MessageResponseDTO(errorMessage);
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ErrorResponseDTO(errorMessage), HttpStatus.BAD_REQUEST);
         }
-
-        MessageResponseDTO response = new MessageResponseDTO("Erro de validação");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(CustomAuthenticationException.class)
-    public ResponseEntity<MessageResponseDTO> handleAuthenticationException(CustomAuthenticationException ex) {
-        MessageResponseDTO response = new MessageResponseDTO("Usuário ou senha inválidos");
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        return new ResponseEntity<>(new ErrorResponseDTO("Erro de validação!"), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TokenException.class)
-    public ResponseEntity<MessageResponseDTO> handleTokenException(TokenException ex) {
+    public ResponseEntity<ErrorResponseDTO> handleTokenException(TokenException ex) {
         logger.error(ex.getMessage(), ex);
-        MessageResponseDTO response = new MessageResponseDTO("Ocorreu um erro ao gerar seu token");
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+
+        String mensagem = switch (ex.getHttpStatus()) {
+            case UNAUTHORIZED -> "Token expirado ou inválido.";
+            case FORBIDDEN -> "Acesso negado para esse token.";
+            default -> "Ocorreu um erro ao processar o token.";
+        };
+
+        return new ResponseEntity<>(new ErrorResponseDTO(mensagem), ex.getHttpStatus());
     }
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<MessageResponseDTO> handleProductExists(UserAlreadyExistsException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT) // 409
-                .body(new MessageResponseDTO(ex.getMessage()));
+    @ExceptionHandler(LoginException.class)
+    public ResponseEntity<ErrorResponseDTO> handleLoginException(LoginException ex) {
+        logger.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(new ErrorResponseDTO("Usuário ou senha inválido"), ex.getHttpStatus());
     }
 
 }
