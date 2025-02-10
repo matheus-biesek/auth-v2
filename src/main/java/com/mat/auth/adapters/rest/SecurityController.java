d0fc468cdf12c88ba7ca2fee5084f6e26ce03111
matheus biesek <md.biesek@gmail.com>
2025-02-10 17:33:47 -0300

    Ajusta retorno dos endpoints no SecurityController

--- src/main/java/com/mat/auth/adapters/rest/SecurityController.java
+++ src/main/java/com/mat/auth/adapters/rest/SecurityController.java
@@ -1,18 +1,15 @@
 package com.mat.auth.adapters.rest;
 
-import com.mat.auth.domain.dto.response.LoginResponseDTO;
+import com.mat.auth.domain.dto.response.*;
 import com.mat.auth.domain.port.in.SecurityServicePort;
-import com.mat.auth.domain.dto.request.LoginRequestDTO;
-import com.mat.auth.domain.dto.request.RegisterUserRequestDTO;
+import com.mat.auth.domain.dto.request.UserCredentialsRequestDTO;
 import com.mat.auth.domain.dto.request.UpdateRoleRequestDTO;
 import com.mat.auth.domain.dto.request.UsernameRequestDTO;
-import com.mat.auth.domain.dto.response.TokenResponseDTO;
 import com.mat.auth.domain.enums.UserRole;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,30 +20,26 @@ public class SecurityController {
     private final SecurityServicePort securityService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
-        return ResponseEntity.ok(securityService.login(request.getUsername(), request.getPassword()));
+    public ResponseEntity<TokenResponseDTO> login(@Valid @RequestBody UserCredentialsRequestDTO request) {
+        TokenResponseDTO token  = securityService.login(request.getUsername(), request.getPassword());
+        return ResponseEntity.ok(token);
     }
 
-
-
-
-
-
     @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterUserRequestDTO request) {
-        String token = securityService.registerUserWithRole(request.getUsername(), request.getPassword(), UserRole.USER);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new TokenResponseDTO(token));
+    public ResponseEntity<TokenResponseDTO> register(@Valid @RequestBody UserCredentialsRequestDTO request) {
+        TokenResponseDTO token = securityService.registerUserWithRole(request.getUsername(), request.getPassword(), UserRole.USER);
+        return new ResponseEntity<>(token, HttpStatus.CREATED);
     }
 
     @PatchMapping("/update-role")
-    public ResponseEntity<?> updateRole(@Valid @RequestBody UpdateRoleRequestDTO request) {
-        securityService.updateRoleUser(request.getUsername(), request.getRole());
-        return ResponseEntity.ok().build();
+    public ResponseEntity<SuccessResponseDTO> updateRole(@Valid @RequestBody UpdateRoleRequestDTO request) {
+        SuccessResponseDTO messageSuccess = securityService.updateRoleUser(request.getUsername(), request.getRole());
+        return ResponseEntity.ok(messageSuccess);
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteUser(@Valid @RequestBody UsernameRequestDTO request) {
-        securityService.deleteUser(request.getUsername());
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<SuccessResponseDTO> deleteUser(@Valid @RequestBody UsernameRequestDTO request) {
+        SuccessResponseDTO messageSuccess = securityService.deleteUser(request.getUsername());
+        return ResponseEntity.ok(messageSuccess);
     }
 }
