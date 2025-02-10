80a624bb6c015b6aa935f20a8146f5300b09e655
matheus biesek <md.biesek@gmail.com>
2025-02-10 17:30:40 -0300

    Ajustando resposta e exceções do SecurityServiceImpl

--- src/main/java/com/mat/auth/application/service/SecurityServiceImpl.java
+++ src/main/java/com/mat/auth/application/service/SecurityServiceImpl.java
@@ -2,9 +2,9 @@ package com.mat.auth.application.service;
 
 import com.mat.auth.adapters.persistence.UserRepositoryAdapter;
 import com.mat.auth.adapters.rest.exception.LoginException;
-import com.mat.auth.domain.dto.response.LoginResponseDTO;
-import com.mat.auth.domain.exceptions.UserAlreadyExistsException;
-import com.mat.auth.domain.exceptions.UserNotFoundException;
+import com.mat.auth.adapters.rest.exception.SecurityException;
+import com.mat.auth.adapters.rest.exception.registerUserWithRoleException;
+import com.mat.auth.domain.dto.response.*;
 import com.mat.auth.domain.port.in.SecurityServicePort;
 import com.mat.auth.domain.enums.UserRole;
 import com.mat.auth.domain.model.User;
@@ -26,44 +26,46 @@ public class SecurityServiceImpl implements SecurityServicePort {
                 .orElseThrow(() -> exception);
     }
 
-    public LoginResponseDTO login(String username, String password) {
-        User user = findUserByUsername(username, new LoginException("Usuário não encontrado", HttpStatus.UNAUTHORIZED));
+    public TokenResponseDTO login(String username, String password) {
+        User user = findUserByUsername(username, new LoginException("Usuário não encontrado.", HttpStatus.UNAUTHORIZED));
 
         if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new LoginException("Senha inválida", HttpStatus.UNAUTHORIZED);
+            throw new LoginException("Senha inválida.", HttpStatus.UNAUTHORIZED);
         }
 
         String token = tokenServiceImpl.generateToken(user);
-        return new LoginResponseDTO(token);
+        return new TokenResponseDTO(token);
     }
 
-
-
-
-
     @Override
-    public String registerUserWithRole(String username, String password, UserRole role) {
+    public TokenResponseDTO registerUserWithRole(String username, String password, UserRole role) {
         if (userRepositoryAdapter.findByUsername(username).isPresent()) {
-            // criar console global
-            throw new UserAlreadyExistsException("O usuário já existe!");
+            throw new registerUserWithRoleException("Usuário já existe no banco de dados!", HttpStatus.CONFLICT);
         }
+
         User newUser = new User(passwordEncoder.encode(password), username, role);
         userRepositoryAdapter.save(newUser);
-        return tokenServiceImpl.generateToken(newUser);
+
+        String token = tokenServiceImpl.generateToken(newUser);
+        return new TokenResponseDTO(token);
     }
 
     @Override
-    public void updateRoleUser(String username, UserRole role) {
-        User user = userRepositoryAdapter.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException("Usuário não encontrado!"));
+    public SuccessResponseDTO updateRoleUser(String username, UserRole role) {
+        User user = findUserByUsername(username, new SecurityException("Usuário não encontrado.", HttpStatus.NOT_FOUND));
+
         user.setRole(role);
         userRepositoryAdapter.save(user);
+
+        return new SuccessResponseDTO("Role do usuário trocada com sucesso.");
     }
 
     @Override
-    public void deleteUser(String username) {
-        User user = userRepositoryAdapter.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException("Usuário não encontrado!"));
+    public SuccessResponseDTO deleteUser(String username) {
+        User user = findUserByUsername(username, new SecurityException("Usuário não encontrado!", HttpStatus.NOT_FOUND));
+
         userRepositoryAdapter.delete(user);
+
+        return new SuccessResponseDTO("Usuário deletado com sucesso.");
     }
 }
--- src/main/java/com/mat/auth/domain/port/in/SecurityServicePort.java
+++ src/main/java/com/mat/auth/domain/port/in/SecurityServicePort.java
@@ -1,11 +1,11 @@
 package com.mat.auth.domain.port.in;
 
-import com.mat.auth.domain.dto.response.LoginResponseDTO;
+import com.mat.auth.domain.dto.response.*;
 import com.mat.auth.domain.enums.UserRole;
 
 public interface SecurityServicePort {
-    public LoginResponseDTO login(String username, String password);
-    public String registerUserWithRole(String username, String password, UserRole role);
-    public void updateRoleUser(String username, UserRole role);
-    public void deleteUser(String username);
+    public TokenResponseDTO login(String username, String password);
+    public TokenResponseDTO registerUserWithRole(String username, String password, UserRole role);
+    public SuccessResponseDTO updateRoleUser(String username, UserRole role);
+    public SuccessResponseDTO deleteUser(String username);
 }
